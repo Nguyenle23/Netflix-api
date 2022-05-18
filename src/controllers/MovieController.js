6297e27284ec933f268e2c9b3f04b36a6efb83ef
Nguyenle23 <beenbin2308@gmail.com>
2022-05-18 08:31:02 +0700

    Remove

--- src/controllers/MovieController.js
+++ src/controllers/MovieController.js
@@ -67,25 +67,5 @@ const findAllMovie = async(req, res) => {
     }
 }
 
-const findRandomMovie = async(req, res) => {
-    const type = req.query.type;
-    let movie;
-    try {
-        if (type === "series") {
-            movie = await Movie.aggregate([
-                { $match: { isSeries: true } },
-                { $sample: { size: 1 } },
-            ]);
-        } else {
-            movie = await Movie.aggregate([
-                { $match: { isSeries: false } },
-                { $sample: { size: 1 } },
-            ]);
-        }
-        res.status(200).json(movie);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-}
 
-module.exports = { createMovie, updateMovie, deleteMovie, findMovie, findAllMovie, findRandomMovie };
+module.exports = { createMovie, updateMovie, deleteMovie, findMovie, findAllMovie };
--- src/routes/movie.js
+++ src/routes/movie.js
@@ -10,7 +10,6 @@ router.put('/:id', verify, movieController.updateMovie);
 router.delete('/:id', verify, movieController.deleteMovie);
 router.get('/find/:id', verify, movieController.findMovie);
 router.get('/', verify, movieController.findAllMovie);
-router.get('/random', verify, movieController.findRandomMovie);
 
 
 module.exports = router;
